b221712a6f9b7a94a623f3929a0d5e2fc2ac0a6e
Alejandro Quiñones Caicedo <50637445+WholeMuffin@users.noreply.github.com>
2021-12-05 16:46:54 -0500

    Ajustes de contenido

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -3,10 +3,8 @@ import React from 'react';
 const Index = () => {
   return (
     <div>
-      <div className='bg-green-300 h-96'>Index page</div>
-      <div className='bg-green-300 h-96'>Index page</div>
-      <div className='bg-green-300 h-96'>Index page</div>
-      <div className='bg-green-300 h-96'>Index page</div>
+      <div className='bg-blue-300 h-96'>Contenido de prueba</div>
+      <div className='bg-green-300 h-96'>Insertar texto</div>
     </div>
   );
 };
