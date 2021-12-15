78af6749462cd0f8ca6c280e23c0f39b5a577fc3
GaboSof <sofia_paez_castro@hotmail.es>
2021-12-15 00:13:07 -0500

    Visual front

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
+import inicio from './imagenes/inicio.jpg'
 
 const Index = () => {
   return (
     <div>
-      <div className='bg-blue-300 h-96'>Contenido de prueba</div>
-      <div className='bg-green-300 h-96'>Insertar texto</div>
+      <img src={inicio}/>
     </div>
   );
 };
